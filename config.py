57caaf34f819e55b042189d18cf4c5cfe2b8a9ba
Ivan Mactaggart <ivan@cambridgepicture.com>
2026-05-19 14:47:39 +0000

    Change to shared M365 authentication across apps

--- config.py
+++ config.py
@@ -12,6 +12,5 @@ BASE_DIR = str(BASE_PATH)
 SECRET_KEY = os.getenv('SECRET_KEY', 'change-me-in-production')
 STATIC_URL_PATH = '/static'
 USER_DB_PATH = str(BASE_PATH / 'data' / '.user_store.sqlite3')
-TRANSLATE_APP_URL = os.getenv('TRANSLATE_APP_URL', 'https://app.cambridgepicture.com/translate/')
 LOGIN_USERNAME = os.getenv('LOGIN_USERNAME', 'ivan@cambridgepicture.com')
 LOGIN_PASSWORD = os.getenv('LOGIN_PASSWORD', 'replace-with-your-password')
